e5209071b9d5d6262594650c1cbd85c2d0a61e78
Krzysiek Setlak <bigfoot19@wp.pl>
2024-10-21 20:18:01 +0200

    A "cool" mouseover effect for my name in the header.

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-// Nothing to see here.
+const h1_name = document.querySelector("#h1-name");
+
+h1_name.addEventListener("mouseenter", (event) => {
+  event.target.innerHTML = "&#127477;&#127473; Krzysztof (/ˈkʂɘʂ.tɔf/)";
+});
+
+h1_name.addEventListener("mouseleave", (event) => {
+  event.target.innerHTML = "Chris";
+});
